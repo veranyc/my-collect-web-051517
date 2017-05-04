738625b426bdfa2a72783024dd0bda12d54d89dc
Vera Zago <veranyc@gmail.com>
2017-05-04 13:38:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 def my_collect(collection)
-  i = 0
-  new_collection = []
+  i = 0 #counter
+  new_collection = [] #need a new array
   while i < collection.length
+    #run a block of code for each element in the collection and then add the modified element to the new array
     new_collection << (yield(collection[i]))
-    i += 1
+    i += 1 #increase counter by 1
   end
-  new_collection
+  new_collection #return the new array
 end
